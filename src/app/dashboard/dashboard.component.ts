a82fcdebb21d54ea7fa2fccfebf60a52c6c47fac
FlorinaVatamanu <75433701+FlorinaVatamanu@users.noreply.github.com>
2022-06-20 12:47:21 +0300

    Add files via upload

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,31 +1,127 @@
 import { Component, OnInit } from '@angular/core';
-
-import { AngularFireAuth } from '@angular/fire/compat/auth';
-import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/compat/firestore';
-import { Observable } from 'rxjs';
+import { Food } from '../model/food';
+import { AuthService } from '../services/auth.service';
+import { DataService } from '../services/data.service';
 
 @Component({
-    selector: 'app-dashboard',
-    templateUrl: './dashboard.component.html',
-    styleUrls: ['./dashboard.component.scss']
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
 
-    user: Observable<any>;              // Example: store the user's info here (Cloud Firestore: collection is 'users', docId is the user's email, lower case)
+  foodsList: Food[] = [];
 
-    constructor(private afAuth: AngularFireAuth, private firestore: AngularFirestore) {
-        this.user = null;
+  foodObj: Food = {
+    id: '',
+    food_name: '',
+    calories: '',
+    cantity: '',
+    units: '',
+    carbs: '',
+    fat: '',
+    fiber: '',
+    protein: '',
+    sugar: ''
+  };
+  id: string = '';
+  food_name: string = '';
+  calories: string = '';
+  cantity: string = '';
+  units: string = '';
+  carbs: string = '';
+  fat: string = '';
+  fiber: string = '';
+  protein: string = '';
+  sugar: string = '';
+
+  constructor(private auth: AuthService, private data: DataService) { }
+
+  ngOnInit(): void {
+    this.getAllFoods();
+  }
+
+  // register() {
+  //   this.auth.logout();
+  // }
+
+  getAllFoods() {
+
+    this.data.getAllFoods().subscribe(res => {
+
+      this.foodsList = res.map((e: any) => {
+        const data = e.payload.doc.data();
+        data.id = e.payload.doc.id;
+        return data;
+      })
+
+    }, err => {
+      alert('Error while fetching foods data.');
+    })
+
+  }
+
+  resetForm() {
+    this.id = '';
+    this.food_name = '';
+    this.calories = '';
+    this.cantity = '';
+    this.units = '';
+    this.carbs = '';
+    this.fat = '';
+    this.fiber = '';
+    this.protein = '';
+    this.sugar = '';
+  }
+
+  addFood() {
+    if (this.food_name == '' ||  this.calories == '' || this.cantity == '' || this.units == '' || this.carbs == '' || this.fat == '' || this.fiber == '' || this.protein == '' || this.sugar == '' ) {
+      alert('Fill all input fields.');
+      return;
     }
 
-    ngOnInit(): void {
-        this.afAuth.authState.subscribe(user => {
-            console.log('Dashboard: user', user);
+      this.foodObj.id = '';
+      this.foodObj.food_name = this.food_name;
+      this.foodObj.calories = this.calories;
+      this.foodObj.cantity = this.cantity;
+      this.foodObj.units = this.units;
+      this.foodObj.carbs = this.carbs;
+      this.foodObj.fat = this.fat;
+      this.foodObj.fiber = this.fiber;
+      this.foodObj.protein = this.protein;
+      this.foodObj.sugar = this.sugar;
 
-            if (user) {
-                let emailLower = user.email.toLowerCase();
-                this.user = this.firestore.collection('users').doc(emailLower).valueChanges();
-            }
-        });
+      this.data.addFood(this.foodObj);
+      this.resetForm();
+
+  }
+
+  updateFood(food: Food) {
+    if (this.food_name == '' ||  this.calories == '' || this.cantity == '' || this.units == '' || this.carbs == '' || this.fat == '' || this.fiber == '' || this.protein == '' || this.sugar == '' ) {
+      alert('Fill all input fields.');
+      return;
     }
+      this.data.deleteFood(food);
+
+      this.foodObj.id = '';
+      this.foodObj.food_name = this.food_name;
+      this.foodObj.calories = this.calories;
+      this.foodObj.cantity = this.cantity;
+      this.foodObj.units = this.units;
+      this.foodObj.carbs = this.carbs;
+      this.foodObj.fat = this.fat;
+      this.foodObj.fiber = this.fiber;
+      this.foodObj.protein = this.protein;
+      this.foodObj.sugar = this.sugar;
+
+      this.data.addFood(this.foodObj);
+      this.resetForm();
+  }
+
+  deleteFood(food: Food) {
+    if (window.confirm('Are you sure you want to delete ' + food.food_name + ' ?')) {
+      this.data.deleteFood(food);
+    }
+  }
+
 }
-
