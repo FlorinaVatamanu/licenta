e2240144b045616a8aa625c9f2095c88c154099c
FlorinaVatamanu <75433701+FlorinaVatamanu@users.noreply.github.com>
2022-06-20 12:49:38 +0300

    Add files via upload

---
+++ src/app/bmr/bmr.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bmr',
+  templateUrl: './bmr.component.html',
+  styleUrls: ['./bmr.component.scss']
+})
+export class BmrComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
